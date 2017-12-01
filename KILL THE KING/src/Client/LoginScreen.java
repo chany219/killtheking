52a92c0d4a9ae29d4726fc019b84706ee760d333
Namkangwoo <32699378+Namkangwoo@users.noreply.github.com>
2017-12-01 17:44:07 +0900

    Update

--- KILL THE KING/src/Client/LoginScreen.java
+++ KILL THE KING/src/Client/LoginScreen.java
@@ -5,17 +5,16 @@ import javax.swing.*;
 
 public class  LoginScreen extends JFrame{
 
-	Container content;
-	ImagePanel imgPanel;
+   Container content;
+   ImagePanel imgPanel;
 
-	public LoginScreen(String title){
-		super(title);
-		imgPanel = new ImagePanel();
-		content = getContentPane();     
-		content.add(imgPanel, BorderLayout.CENTER);
-		setSize(1050,600);
-		setLocation(600, 200);
-		setVisible(true);
-	}
+   public LoginScreen(String title){
+      super(title);
+      imgPanel = new ImagePanel();
+      content = getContentPane();     
+      content.add(imgPanel, BorderLayout.CENTER);
+      setSize(1050,600);
+      setLocation(600, 200);
+      setVisible(true);
+   }
 } 
-
