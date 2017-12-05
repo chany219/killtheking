f47f02e8c61494b34c3ade0263f6030b53fbf431
Namkangwoo <32699378+Namkangwoo@users.noreply.github.com>
2017-12-06 00:23:21 +0900

    Add files via upload

--- KILL THE KING/src/Client/ButtonPanel.java
+++ KILL THE KING/src/Client/ButtonPanel.java
@@ -11,82 +11,99 @@ import javax.swing.JPanel;
 
 public class ButtonPanel extends JPanel  {
 
-   public static boolean flag=true;
-   public static String direction;
+	public static boolean flag=true;
+	public static String direction;
 
-   Button []btn=new Button[6];
-   ButtonPanel(){
-      setOpaque(false);
-      setLayout(new GridLayout(2,3));
-      btn[0]=new Button(" ");
-      btn[1]=new Button("¡è");
-      btn[2]=new Button(" ");
-      btn[3]=new Button("¡ç");
-      btn[4]=new Button("¡é");
-      btn[5]=new Button("¡æ");
+	Button []btn=new Button[6];
+	ButtonPanel(){
+		setOpaque(false);
+		setLayout(new GridLayout(2,3));
+		btn[0]=new Button(" ");
+		btn[1]=new Button("¡è");
+		btn[2]=new Button(" ");
+		btn[3]=new Button("¡ç");
+		btn[4]=new Button("¡é");
+		btn[5]=new Button("¡æ");
 
-      for(int i=0;i<btn.length;i++)
-      {
-         add(btn[i]);
-         btn[i].setFont(new Font("°íµñ,",Font.BOLD,30));
-         if(i!=0&&i!=2)
-            btn[i].setBackground(Color.YELLOW);
-         else
-            btn[i].setBackground(Color.WHITE);
-      }
+		for(int i=0;i<btn.length;i++)
+		{
+			add(btn[i]);
+			btn[i].setFont(new Font("°íµñ,",Font.BOLD,30));
+			if(i!=0&&i!=2)
+				btn[i].setBackground(Color.WHITE);
+			else
+				btn[i].setBackground(Color.GRAY);
+			btn[i].setEnabled(false);
+		}
 
-      ActionListener btn1=new ActionListener(){
 
-         public void actionPerformed(ActionEvent e){
-            if(flag)
-            {
-               btn[1].setBackground(Color.WHITE);
+		ActionListener btn1=new ActionListener(){
 
-               direction="up";
-               flag=false;
-            }
-         }
-      };
-      btn[1].addActionListener(btn1);
+			public void actionPerformed(ActionEvent e){
+				if(flag)
+				{
+					btn[1].setBackground(Color.YELLOW);
 
-      ActionListener btn3=new ActionListener(){
+					direction="up";
+					flag=false;
+				}
+			}
+		};
+		btn[1].addActionListener(btn1);
 
-         public void actionPerformed(ActionEvent e){
-            if(flag)
-            {
-               btn[3].setBackground(Color.WHITE);
-               direction="left";
-               flag=false;
-            }
-         }
-      };
+		ActionListener btn3=new ActionListener(){
 
-      btn[3].addActionListener(btn3);
+			public void actionPerformed(ActionEvent e){
+				if(flag)
+				{
+					btn[3].setBackground(Color.YELLOW);
+					direction="left";
+					flag=false;
+				}
+			}
+		};
 
-      ActionListener btn4=new ActionListener(){
-         public void actionPerformed(ActionEvent e){
-            if(flag)
-            {
-               btn[4].setBackground(Color.WHITE);
-               direction="down";
-               flag=false;
-            }
-         }
-      };
-      btn[4].addActionListener(btn4);
-      ActionListener btn5=new ActionListener(){
-         public void actionPerformed(ActionEvent e){
-            if(flag)
-            {
-               btn[5].setBackground(Color.WHITE);
-               direction="right";
-               flag=false;
-            }
-         }
-      };
-      btn[5].addActionListener(btn5);
-   }
+		btn[3].addActionListener(btn3);
 
+		ActionListener btn4=new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				if(flag)
+				{
+					btn[4].setBackground(Color.YELLOW);
+					direction="down";
+					flag=false;
+				}
+			}
+		};
+		btn[4].addActionListener(btn4);
+		ActionListener btn5=new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				if(flag)
+				{
+					btn[5].setBackground(Color.YELLOW);
+					direction="right";
+					flag=false;
+				}
+			}
+		};
+		btn[5].addActionListener(btn5);
+	}
+	public void setEnabledButton(int a)
+	{
+		for(int i=0;i<btn.length;i++)
+			if(a==1)
+			btn[i].setEnabled(true);
+			else
+				btn[i].setEnabled(false);
+	}
+	public void setButtonBackground()
+	{
+		for(int i=0;i<btn.length;i++)
+		{
+			if(i!=0&&i!=2)
+				btn[i].setBackground(Color.WHITE);
+		}
+	}
 }
 
 
